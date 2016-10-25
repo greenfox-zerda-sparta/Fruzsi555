b7c3c211ef4b7edd3993395e15ca03bf7e469153
Fruzsi555 <karman.fruzsina@gmail.com>
2016-10-25 17:57:44 +0200

    Ex.16.d.cpp

---
+++ week-02/day-2/16.d.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int m[3][3] = { { 1, 2, 3 }, { 4, 5, 6 }, { 7, 8, 9 } };
+	//This is an array of arrays. Print it out as a 3x3 matrix. Using each inner array as
+	//d.) all in one column
+
+	int i, j;
+
+	for (i = 0; i < 3; i++) {
+		for (j = 0; j < 3; j++) {
+			printf("%d ", m[i][j]);
+			//cout << m[i][j]);
+			cout << endl;
+		}
+		cout << endl;
+	}
+	return 0;
+}
