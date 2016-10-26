ccbef618091eb58e4bb9b0b965e6ef346f13851a
Fruzsi555 <karman.fruzsina@gmail.com>
2016-10-26 17:39:35 +0200

    Pointers ex 01-05

---
+++ week-02/day-3/05.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  int numbers[] = {5, 6, 7, 8, 9};
+  int* number_pointer = &numbers[0];
+  // The "number_pointer" should point to the third element of the array called "numbers",
+  // then please print its value with it. Solve the problem without reusing the "&" operator
+  // for getting the address of the third element.
+
+  cout << *(number_pointer +=2) << endl;
+  cout << *number_pointer;
+
+  return 0;
+}
