9d6095ac78962ba5337d11283439bc956d9868a6
DESKTOP-3SNT22J\fruzsinakarman <karman.fruzsina@gmail.com>
2017-01-24 16:33:16 +0100

    First C# project with MVVM -Model: User.cs -View: MainWindow.xaml -ViewModel: MainViewModel.cs

---
+++ week-12/MVVM_Users/MainViewModel.cs
@@ -0,0 +1,58 @@
+using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.Command;
+using System.Collections.ObjectModel;
+using System;
+using GalaSoft.MvvmLight.Messaging;
+using System.Windows.Input;
+
+namespace SimpleMVVM.ViewModel
+{
+    /// <summary>
+    /// This class contains properties that the main View can data bind to.
+    /// <para>
+    /// Use the <strong>mvvminpc</strong> snippet to add bindable properties to this ViewModel.
+    /// </para>
+    /// <para>
+    /// You can also use Blend to data bind with the tool's support.
+    /// </para>
+    /// <para>
+    /// See http://www.galasoft.ch/mvvm
+    /// </para>
+    /// </summary>
+    public class MainViewModel : ViewModelBase
+    {
+        private ObservableCollection<User> users = new ObservableCollection<User>();
+        private User selectedUser;
+
+        public ICommand LoadUsersCommand { get; private set; }
+
+        public ObservableCollection<User> Users
+        {
+            get { return users; }
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the MainViewModel class.
+        /// </summary>
+        public MainViewModel()
+        {
+            LoadUsersCommand = new RelayCommand(LoadUsersCommandHandler);
+        }
+
+        public User SelectedUser
+        {
+            get { return selectedUser; }
+            set {
+                selectedUser = value;
+                RaisePropertyChanged("SelectedUser");
+            }
+        }
+
+        private void LoadUsersCommandHandler()
+        {
+            users = User.GenerateDummyUsers();
+            this.RaisePropertyChanged(() => Users);
+            Messenger.Default.Send<NotificationMessage>(new NotificationMessage("Users Loaded."));
+        }
+    }
+}
